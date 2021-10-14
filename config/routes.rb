d307c39f1da61289c715f14e3b03647951f5290f
sayahaya <sayahaya1129@gmail.com>
2021-10-14 21:08:41 +0900

    Add: ユーザー詳細ページ追加 #11

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  resources :users, only: %i[create new index]
+  resources :users, only: %i[create new index show]
 end
