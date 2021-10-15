17cd98b0add4da4a290c2280f7339a58b7e9cda9
sayahaya <sayahaya1129@gmail.com>
2021-10-15 14:55:33 +0900

    Add: seed.rbにテストユーザー追加 #13

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+30.times do |i|
+  User.create!(
+    name: "test#{i}",
+    email: "test#{i}@example.com",
+    password: 12345678,
+    password_confirmation: 12345678
+  )
+end
