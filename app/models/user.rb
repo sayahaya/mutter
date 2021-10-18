e5dd237252e3195bf41894b51338048016e3db75
sayahaya <sayahaya1129@gmail.com>
2021-10-18 10:53:26 +0900

    Add: passive_relationships,follwers #17

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :mutters, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :following, through: active_relationships, source: followed
+  has_many :passive_relationships, class_name: "Relaationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followes, through: :passive_relationships, source: :follower
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
