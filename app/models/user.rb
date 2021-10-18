92cac3f492003216fe0b70c95e44b7fa57e045db
sayahaya <sayahaya1129@gmail.com>
2021-10-18 14:03:19 +0900

    Add: フォロー,フォロワー数の記載追加 #17

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :mutters, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :passive_relationships, class_name: "Relaationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
