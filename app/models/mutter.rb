33495572e18e8b91fa3715d5ab879d7f91a667ac
sayahaya <sayahaya1129@gmail.com>
2021-10-19 22:29:49 +0900

    Fix: user.rbとmutter.rbの記載間違いを修正 #19

--- app/models/mutter.rb
+++ app/models/mutter.rb
@@ -1,7 +1,7 @@
 class Mutter < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
-  has_many :like_users, through: likes, source: user
+  has_many :like_users, through: :likes, source: :user
   default_scope { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :likes, dependent: :destroy
-  has_many :like_mutters, through: likes, source: mutter
+  has_many :like_mutters, through: :likes, source: :mutter
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
