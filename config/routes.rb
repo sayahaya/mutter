9ed950008b8c575709e34edf0383f9ceac00aadf
sayahaya <sayahaya1129@gmail.com>
2021-11-02 17:58:25 +0900

    fix: about contact helpページ削除 #40

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get 'help', to: 'static_pages#help'
-  get 'about', to: 'static_pages#about'
-  get 'contact', to: 'static_pages#contact'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
